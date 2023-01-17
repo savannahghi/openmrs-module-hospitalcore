62aaec1b6ec146cd7e88ef23495c225eebb33f57
Stanslaus Odhiambo <mstans8@gmail.com>
2023-01-17 22:59:52 +0300

    add default regimen mapping to starter drugs

--- api/src/main/java/org/openmrs/module/hospitalcore/model/RegimenType.java
+++ api/src/main/java/org/openmrs/module/hospitalcore/model/RegimenType.java
@@ -3,12 +3,15 @@ package org.openmrs.module.hospitalcore.model;
 import org.openmrs.BaseOpenmrsData;
 
 import java.io.Serializable;
+import java.util.Set;
 
 public class RegimenType extends BaseOpenmrsData implements Serializable {
     private static final long serialVersionUID = 1L;
     private Integer id;
     private String  name;
     private Integer cycles;
+
+    private Set<DefaultRegimenMapping> regimenMappings;
 
     public Integer getId() {
         return id;
@@ -32,5 +35,13 @@ public class RegimenType extends BaseOpenmrsData implements Serializable {
 
     public void setCycles(Integer cycles) {
         this.cycles = cycles;
+    }
+
+    public Set<DefaultRegimenMapping> getRegimenMappings() {
+        return regimenMappings;
+    }
+
+    public void setRegimenMappings(Set<DefaultRegimenMapping> regimenMappings) {
+        this.regimenMappings = regimenMappings;
     }
 }
